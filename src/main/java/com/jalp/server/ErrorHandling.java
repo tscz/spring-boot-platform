aa5d3498eeb25b37fb1d6cefbfb0c75c0f61c5e4
tscz <tscz@users.noreply.github.com>
2019-08-18 20:49:12 +0900

    Renaming Error Handler class

--- src/main/java/com/jalp/server/ErrorHandling.java
+++ src/main/java/com/jalp/server/ErrorHandling.java
@@ -10,18 +10,23 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.zalando.problem.Problem;
 import org.zalando.problem.ThrowableProblem;
-import org.zalando.problem.spring.web.advice.AdviceTrait;
 import org.zalando.problem.spring.web.advice.ProblemHandling;
 import org.zalando.problem.spring.web.advice.security.SecurityAdviceTrait;
 
 /**
- * Overrides default error controller from spring, because Exceptions thrown in
- * {@link javax.servlet.Filter} are not handled in
- * {@link org.springframework.web.bind.annotation.ExceptionHandler}.
+ * Error Handling for Spring Boot App:
+ * 
+ * <li>Overrides default error controller from spring, because Exceptions thrown
+ * in {@link javax.servlet.Filter} are not handled in
+ * {@link org.springframework.web.bind.annotation.ExceptionHandler}.</li>
+ * 
+ * <li>Add {@link ExceptionHandler}s, which are implementing
+ * {@link https://tools.ietf.org/html/rfc7807} for standardized
+ * application/problem+json rest responses. based on</li>
  */
 @RestController
 @ControllerAdvice
-public class CustomErrorController implements ErrorController, AdviceTrait, ProblemHandling, SecurityAdviceTrait {
+public class ErrorHandling implements ErrorController, ProblemHandling, SecurityAdviceTrait {
 
 	@RequestMapping(value = "/error")
 	Problem error(HttpServletRequest request, HttpServletResponse response) throws Throwable {
