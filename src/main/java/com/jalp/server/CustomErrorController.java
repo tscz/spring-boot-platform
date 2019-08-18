4ca109779f0642b54b6c9e5b3573f2014f653ce9
tscz <tscz@users.noreply.github.com>
2019-08-18 20:41:03 +0900

    Centralize Error Handling/Mapping

--- src/main/java/com/jalp/server/CustomErrorController.java
+++ src/main/java/com/jalp/server/CustomErrorController.java
@@ -5,11 +5,14 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.zalando.problem.Problem;
 import org.zalando.problem.ThrowableProblem;
 import org.zalando.problem.spring.web.advice.AdviceTrait;
+import org.zalando.problem.spring.web.advice.ProblemHandling;
+import org.zalando.problem.spring.web.advice.security.SecurityAdviceTrait;
 
 /**
  * Overrides default error controller from spring, because Exceptions thrown in
@@ -17,7 +20,8 @@ import org.zalando.problem.spring.web.advice.AdviceTrait;
  * {@link org.springframework.web.bind.annotation.ExceptionHandler}.
  */
 @RestController
-public class CustomErrorController implements ErrorController, AdviceTrait {
+@ControllerAdvice
+public class CustomErrorController implements ErrorController, AdviceTrait, ProblemHandling, SecurityAdviceTrait {
 
 	@RequestMapping(value = "/error")
 	Problem error(HttpServletRequest request, HttpServletResponse response) throws Throwable {
--- src/main/java/com/jalp/server/ExceptionHandler.java
+++
@@ -1,9 +0,0 @@
-package com.jalp.server;
-
-import org.springframework.web.bind.annotation.ControllerAdvice;
-import org.zalando.problem.spring.web.advice.ProblemHandling;
-
-@ControllerAdvice()
-public class ExceptionHandler implements ProblemHandling {
-
-}
