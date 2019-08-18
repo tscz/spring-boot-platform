6c4e768b997ea6a17e31bba320d6d30caf75b34d
tscz <tscz@users.noreply.github.com>
2019-08-18 20:33:04 +0900

    Update Tests

--- src/main/java/com/jalp/server/model/Vocable.java
+++ src/main/java/com/jalp/server/model/Vocable.java
@@ -50,4 +50,41 @@ public class Vocable {
 	public String toString() {
 		return "Vocable [id=" + id + ", value=" + value + ", translation=" + translation + "]";
 	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((translation == null) ? 0 : translation.hashCode());
+		result = prime * result + ((value == null) ? 0 : value.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Vocable other = (Vocable) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (translation == null) {
+			if (other.translation != null)
+				return false;
+		} else if (!translation.equals(other.translation))
+			return false;
+		if (value == null) {
+			if (other.value != null)
+				return false;
+		} else if (!value.equals(other.value))
+			return false;
+		return true;
+	}
 }
